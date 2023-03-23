23c67e588dd461e6ef49e7bc94a4ca55698b21a4
RodrigoMarques23 <45172954+RodrigoMarques23@users.noreply.github.com>
2023-03-23 16:14:03 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace testrepo
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello stranger!");
             SomeFunction();
         }
 
